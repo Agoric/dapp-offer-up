00a044c1efc21cde86bf2c262d8103b67858b9b4
Dan Connolly <dckc@madmode.com>
2023-12-05 15:45:52 -0600

    feat: install bundles automatically
     - capture bundle filenames when building proposal
     - build proposal _inside_ container

---
+++ contract/scripts/install-bundles.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# NOTE: intended to run _inside_ the agd container
+
+set -xueo pipefail
+
+cd /workspace/contract
+
+# TODO: try `agoric publish` to better track outcome
+install_bundle() {
+  ls -sh "$1"
+  agd tx swingset install-bundle --compress "@$1" \
+    --from user1 --keyring-backend=test --gas=auto \
+    --chain-id=agoriclocal -bblock --yes -o json
+}
+
+# exit fail if bundle-list is emtpy
+[ -s bundles/bundle-list ] || exit 1
+
+# TODO: avoid needing so much IST
+# https://github.com/agoric-labs/dapp-game-places/issues/3
+make mint4k
+make mint4k
+make balance-q  # do we have enough IST?
+
+for b in $(cat bundles/bundle-list); do 
+  echo installing $b
+  install_bundle $b
+done
