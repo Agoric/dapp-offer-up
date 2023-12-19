9f40cc085aa986c5183b0bc0812845914611ee48
Dan Connolly <dckc@madmode.com>
2023-12-19 10:30:40 -0600

    fix(ui): prune logo, HMR hints

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -198,11 +198,7 @@ function App() {
           </div>
         </div>
         <button onClick={onClick}>{buttonLabel}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">Click on the logos to learn more</p>
     </>
   );
 }
