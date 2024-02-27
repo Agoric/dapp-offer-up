9690c4b43f74db8ddd1c54ccc15360857ac71eef
rabi-siddique <rabisiddique27@gmail.com>
2024-02-27 16:20:05 +0500

    chore: remove  unused @ts-expect-error

--- ui/src/installSesLockdown.ts
+++ ui/src/installSesLockdown.ts
@@ -3,7 +3,6 @@ import '@endo/eventual-send/shim.js'; // adds support needed by E
 
 const consoleTaming = import.meta.env.DEV ? 'unsafe' : 'safe';
 
-// @ts-expect-error global
 lockdown({
   errorTaming: 'unsafe',
   overrideTaming: 'severe',
