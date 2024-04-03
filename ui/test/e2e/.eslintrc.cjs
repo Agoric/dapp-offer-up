135a05d5c58e43742db2599b74f39f5f36d6721d
rabi-siddique <rabisiddique27@gmail.com>
2024-04-03 20:52:32 +0500

    chore: updating path for synpress .eslintrc.js file

--- ui/test/e2e/.eslintrc.cjs
+++ ui/test/e2e/.eslintrc.cjs
@@ -1,5 +1,9 @@
 const path = require('path');
-const synpressPath = path.join(process.cwd(), '/node_modules/@agoric/synpress');
+const cwd = process.cwd();
+const directories = cwd.split(path.sep);
+directories.pop();
+const updatedPath = directories.join(path.sep);
+const synpressPath = path.join(updatedPath, '/node_modules/@agoric/synpress');
 
 module.exports = {
   extends: `${synpressPath}/.eslintrc.js`,
