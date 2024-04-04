e8a06006d1b870e2e0177f5bde110c9b37dfa987
rabi-siddique <rabisiddique27@gmail.com>
2024-04-05 02:03:25 +0500

    ci: adding implementation for running e2e tests in Github Actions runner

--- contract/scripts/send-funds.sh
+++
@@ -1,6 +0,0 @@
-src=agoric1t83za2h5zc5anmkxvfag82qafshr538mvzmnmx
-dest=agoric1p2aqakv3ulz4qfy2nut86j9gx0dx0yw09h96md
-amt=5553530000uist
-
-agd tx bank send $src $dest $amt --keyring-backend=test --chain-id=agoriclocal \
---gas=auto --gas-adjustment=1.2 --yes -b block
--- ui/test/e2e/specs/test.spec.js
+++ ui/test/e2e/specs/test.spec.js
@@ -3,7 +3,13 @@
 describe('DAPP Offer Up E2E Test Cases', () => {
   context('Test commands', () => {
     it(`should complete Keplr setup by  importing an existing wallet using 24 word phrase`, () => {
-      cy.setupWallet().then((setupFinished) => {
+      cy.setupWallet({
+        secretWords:
+          'tackle hen gap lady bike explain erode midnight marriage wide upset culture model select dial trial swim wood step scan intact what card symptom',
+        password: 'Test1234',
+        newAccount: true,
+        walletName: 'My Wallet 2',
+      }).then(setupFinished => {
         expect(setupFinished).to.be.true;
       });
       cy.visit('/');
@@ -13,17 +19,18 @@ describe('DAPP Offer Up E2E Test Cases', () => {
       cy.on('window:alert', alertShown);
 
       cy.contains('Connect Wallet').click();
-      cy.rejectAccess().then((rejected) => {
+      cy.rejectAccess().then(rejected => {
         expect(rejected).to.be.true;
       });
       cy.get('@alertShown').should(
         'have.been.calledOnceWith',
-        'Request rejected'
+        'Request rejected',
       );
     });
     it(`should accept connection with wallet`, () => {
+      cy.reload();
       cy.contains('Connect Wallet').click();
-      cy.acceptAccess().then((taskCompleted) => {
+      cy.acceptAccess().then(taskCompleted => {
         expect(taskCompleted).to.be.true;
       });
     });
@@ -32,27 +39,29 @@ describe('DAPP Offer Up E2E Test Cases', () => {
       cy.on('window:alert', alertShown);
 
       cy.contains('Make an Offer').click();
-      cy.rejectTransaction().then((taskCompleted) => {
+      cy.rejectTransaction().then(taskCompleted => {
         expect(taskCompleted).to.be.true;
       });
 
       cy.get('@alertShown').should(
         'have.been.calledOnceWith',
-        'Offer error: Error: Request rejected'
+        'Offer error: Error: Request rejected',
       );
     });
     it(`should confirm make an offer transaction`, () => {
+      cy.reload();
+      cy.contains('Connect Wallet').click();
       const alertShown = cy.stub().as('alertShown');
       cy.on('window:alert', alertShown);
 
       cy.contains('Make an Offer').click();
-      cy.confirmTransaction().then((taskCompleted) => {
+      cy.confirmTransaction().then(taskCompleted => {
         expect(taskCompleted).to.be.true;
       });
 
       cy.get('@alertShown').should(
         'have.been.calledOnceWith',
-        'Offer accepted'
+        'Offer accepted',
       );
     });
   });
